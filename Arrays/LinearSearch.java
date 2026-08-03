8b8d9e8a619fac631aac18b9e28fa12667adb2c1
pragathishetty <54884601+ShettyDhanu@users.noreply.github.com>
2026-08-03 23:33:56 +0530

    Array LinearSearch

---
+++ Arrays/LinearSearch.java
@@ -0,0 +1,14 @@
+Check if number 18 exist in a array
+
+int[] numbers = {25, 10, 45, 18, 32};
+
+int target = 18;
+
+for (int i = 0; i < numbers.length; i++) {
+
+    if (numbers[i] == target) {
+        System.out.println("18 exists");
+break;
+    }
+
+}
