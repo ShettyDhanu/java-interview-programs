e7dcd9a3b766380dbbfebdd9b4d5325232d6838e
pragathishetty <54884601+ShettyDhanu@users.noreply.github.com>
2026-08-04 23:58:12 +0530

    comments updated for linear search

--- Arrays/LinearSearch.java
+++ Arrays/LinearSearch.java
@@ -1,4 +1,13 @@
 Check if number 18 exist in a array
+    /*
+Program: Linear Search
+Concepts:
+1. Arrays
+2. for loop
+3. if condition
+4. boolean flag
+5. break
+*/
 
 int[] numbers = {25, 10, 45, 18, 32};
 
